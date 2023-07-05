f9e16a76c7d5379442240f53c441cb7b6f9dc87f
vinayakvaghani <vinayakvaghani@gmail.com>
2023-07-05 18:52:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello vinayak</h1>
+     <table border={2}>
+      <tr>
+        <th>
+          roll no
+        </th>
+        <th>
+          name
+        </th>
+      </tr>
+      <tr>
+        <td>
+          1
+        </td>
+        <td>
+          vinayak
+        </td>
+      </tr>
+      <tr>
+        <td>
+          2
+        </td>
+        <td>
+          arpan
+        </td>
+      </tr>
+      <tr>
+        <td>
+          3
+        </td>
+        <td>
+          tirth
+        </td>
+      </tr>
+     </table>
+    </div>
+  );
+}
+
+export default App;
